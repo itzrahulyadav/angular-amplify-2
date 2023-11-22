34ef90b166f5da448e14fead651c3e5deb82cb43
Rahul Yadav <rahulkyadav9691@gmail.com>
2023-11-22 12:57:52 +0000

    initial commit

---
+++ demo/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,15 @@
+export type AmplifyDependentResourcesAttributes = {
+  "analytics": {
+    "plutus": {
+      "Id": "string",
+      "Region": "string",
+      "appName": "string"
+    }
+  },
+  "auth": {
+    "plutus": {
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string"
+    }
+  }
+}
---
+++ demo/src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { record } from 'aws-amplify/analytics';
+import amplifyconfig from './amplifyconfiguration.json';
+var { Amplify } = require('aws-amplify');
+
+
+Amplify.configure(amplifyconfig);
+
+record({
+  event: {
+    name: 'albumVisit'
+  }
+});
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
